b68e7018c351fc07e31f475bf90af4bdf7275db7
Emily Zhou <zhou@middlebury.edu>
2024-09-13 21:07:48 -0400

    add a new mapbox tile

--- js/index.js
+++ js/index.js
@@ -2,15 +2,18 @@ import { SlideDeck } from './slidedeck.js';
 
 const map = L.map('map', {scrollWheelZoom: false}).setView([0, 0], 0);
 
+const mapboxStyle = 'mapbox/light-v11';
+const mapboxKey = 'pk.eyJ1IjoiZW16aG91IiwiYSI6ImNtMG9henVrdjA2bGwya3EwNWh6OGh1emgifQ.d2Xo2TLSYAGCMqvccySJSA';
 
-const baseTileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZW16aG91IiwiYSI6ImNtMG9henVrdjA2bGwya3EwNWh6OGh1emgifQ.d2Xo2TLSYAGCMqvccySJSA', {
-  maxZoom: 16,
-  attribution: '&copy; <a href="https://www.mapbox.com/" target="_blank">Mapbox</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>',
-  id: 'mapbox/streets-v11', // You can change this to any Mapbox style
-  tileSize: 512,
-  zoomOffset: -1,
-  accessToken: 'pk.eyJ1IjoiZW16aG91IiwiYSI6ImNtMG9henVrdjA2bGwya3EwNWh6OGh1emgifQ.d2Xo2TLSYAGCMqvccySJSA',
-});
+const baseTileLayer = L.tileLayer(
+    `https://api.mapbox.com/styles/v1/${mapboxStyle}/tiles/{z}/{x}/{y}{r}?access_token=${mapboxKey}`,
+    {
+      maxZoom: 16,
+      attribution: '&copy; <a href="https://www.mapbox.com/" target="_blank">Mapbox</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>',
+      tileSize: 512,
+      zoomOffset: -1,
+    },
+);
 baseTileLayer.addTo(map);
 
 // ## Interface Elements
