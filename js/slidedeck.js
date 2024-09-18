49f78d1efe44ebebcb8ed9e20a3e585a3d97be4c
Emily Zhou <zhou@middlebury.edu>
2024-09-17 22:04:32 -0400

    updated slidedeck.js

--- js/slidedeck.js
+++ js/slidedeck.js
@@ -15,6 +15,8 @@ class SlideDeck {
     this.currentSlideIndex = 0;
   }
 
+
+  // eslint-disable-next-line valid-jsdoc
   /**
    * ### updateDataLayer
    *
@@ -27,16 +29,201 @@ class SlideDeck {
    * @return {L.GeoJSONLayer} The new GeoJSON layer that has been added to the
    *                          data layer group.
    */
-  updateDataLayer(data) {
+  updateDataLayer(data, slideID) {
     this.dataLayer.clearLayers();
-    const geoJsonLayer = L.geoJSON(data, {
-      pointToLayer: (p, latlng) => L.marker(latlng),
-      style: (feature) => feature.properties.style,
-    })
-        .bindTooltip((l) => l.feature.properties.label)
-        .addTo(this.dataLayer);
 
-    return geoJsonLayer;
+    if (slideID === 'title-slide') { // first slide map
+      const colorForProdTF = (prodTF) => {
+        return prodTF === 1 ? '#A92D04' : '#DAD8C9';
+      };
+      const geoJsonLayer1 = L.geoJSON(data, {
+        style: (feature) => {
+          const prodTF = feature.properties.prodTF;
+          const fillColor = colorForProdTF(prodTF);
+
+          return {
+            stroke: true,
+            color: '#D3D3D3',
+            weight: 0.5,
+            fillColor: fillColor,
+            fillOpacity: 0.6,
+          };
+        },
+      })
+          .bindTooltip((l) => l.feature.properties._State, {
+            className: 'custom-tooltip',
+          })
+          .addTo(this.dataLayer);
+      return geoJsonLayer1;
+    } else if (slideID === 'second-slide') { // second slide map
+      const val = (value) => {
+        if (value === 0) {
+          return '#DAD8C9';
+        } else if (value > 0 && value < 10000) {
+          return '#FBAF0A';
+        } else if (value >= 10000) {
+          return '#A92D04';
+        }
+      };
+      const geoJsonLayer2 = L.geoJSON(data, {
+        style: (feature) => {
+          const value = feature.properties.value;
+          const fillColor = val(value);
+
+          return {
+            stroke: true,
+            color: '#D3D3D3',
+            weight: 0.5,
+            fillColor: fillColor,
+            fillOpacity: 0.7,
+          };
+        },
+      })
+          .bindTooltip((l) => l.feature.properties._State, {
+            className: 'custom-tooltip',
+          })
+          .addTo(this.dataLayer);
+      return geoJsonLayer2;
+    } else if (slideID === 'third-slide') { // third slide map
+      const val = (value) => {
+        if (value === 0) {
+          return '#DAD8C9';
+        } else if (value > 0 && value < 10000) {
+          return '#FBAF0A';
+        } else if (value >= 10000 && value < 100000) {
+          return '#E75304';
+        } else if (value >= 100000) {
+          return '#A92D04';
+        }
+      };
+      const geoJsonLayer3 = L.geoJSON(data, {
+        style: (feature) => {
+          const value = feature.properties.value;
+          const fillColor = val(value);
+
+          return {
+            stroke: true,
+            color: '#D3D3D3',
+            weight: 0.5,
+            fillColor: fillColor,
+            fillOpacity: 0.7,
+          };
+        },
+      })
+          .bindTooltip((l) => l.feature.properties._State, {
+            className: 'custom-tooltip',
+          })
+          .addTo(this.dataLayer);
+      return geoJsonLayer3;
+    } else if (slideID === 'fourth-slide') { // fourth slide map
+      const val = (value) => {
+        if (value >= 0 && value < 100000) {
+          return '#FBAF0A';
+        } else if (value >= 100000 && value < 200000) {
+          return '#E75304';
+        } else if (value >= 200000 && value <= 2436351) {
+          return '#A92D04';
+        }
+      };
+      const geoJsonLayer4 = L.geoJSON(data, {
+        style: (feature) => {
+          const value = feature.properties.value;
+          const fillColor = val(value);
+
+          return {
+            stroke: true,
+            color: '#E5E4E2',
+            weight: 0.8,
+            fillColor: fillColor,
+            fillOpacity: 0.8,
+          };
+        },
+      })
+          .bindTooltip((l) => l.feature.properties._State, {
+            className: 'custom-tooltip',
+          })
+          .addTo(this.dataLayer);
+      return geoJsonLayer4;
+    } else if (slideID === 'fifth-slide') {
+      const prod = (production) => {
+        if (production >= 0 && production < 10000) {
+          return '#FBAF0A';
+        } else if (production >= 10000 && production < 100000) {
+          return '#E75304';
+        } else if (production >= 100000 && production <= 913802) {
+          return '#A92D04';
+        }
+      };
+      const geoJsonLayer5 = L.geoJSON(data, {
+        style: (feature) => {
+          const production = feature.properties.production;
+          const fillColor = prod(production);
+
+          return {
+            stroke: true,
+            color: '#E5E4E2',
+            weight: 0.8,
+            fillColor: fillColor,
+            fillOpacity: 0.8,
+          };
+        },
+      })
+          .bindTooltip((l) => l.feature.properties._State, {
+            className: 'custom-tooltip',
+          })
+          .addTo(this.dataLayer);
+      return geoJsonLayer5;
+    } else if (slideID === 'sixth-slide') {
+      const prod = (production) => {
+        if (production >= 0 && production < 100000) {
+          return '#FBAF0A';
+        } else if (production >= 100000 && production < 200000) {
+          return '#E75304';
+        } else if (production >= 200000 && production <= 913802) {
+          return '#A92D04';
+        }
+      };
+      const geoJsonLayer6 = L.geoJSON(data, {
+        style: (feature) => {
+          const production = feature.properties.production;
+          const fillColor = prod(production);
+
+          return {
+            stroke: true,
+            color: '#E5E4E2',
+            weight: 0.8,
+            fillColor: fillColor,
+            fillOpacity: 0.8,
+          };
+        },
+      })
+          .bindTooltip((l) => {
+            const name = l.feature.properties.NAME;
+            const production = l.feature.properties.production;
+
+            return `<div class="custom-tooltip">
+                      <strong>${name}</strong><br>
+                      Production: ${production} gallons
+                    </div>`;
+          })
+          .addTo(this.dataLayer);
+      return geoJsonLayer6;
+    } else {
+      const geoJsonLayer = L.geoJSON(data, {
+        style: (feature) => {
+          return {
+            stroke: true,
+            color: '#E5E4E2',
+            weight: 0.8,
+            fillColor: '#A92D04',
+            fillOpacity: 0.8,
+          };
+        },
+      })
+          .bindTooltip((l) => l.feature.properties.name)
+          .addTo(this.dataLayer);
+      return geoJsonLayer;
+    }
   }
 
   /**
@@ -74,42 +261,58 @@ class SlideDeck {
    * @param {HTMLElement} slide The slide's HTML element
    */
   async syncMapToSlide(slide) {
-    const collection = await this.getSlideFeatureCollection(slide);
-    const layer = this.updateDataLayer(collection);
-
-    /**
+    if (slide.id.includes('image')) {
+      // Hide the map and display the image
+      this.map.getContainer().style.display = 'none';
+      const image = document.getElementById('slide-image');
+      image.style.display = 'block';
+      image.src = slide.getAttribute('data-image-src');
+    } else if (slide.id === 'chart-slide') {
+      // Hide the map and show the chart
+      this.map.getContainer().style.display = 'none';
+      const chart = document.getElementById('chart-container');
+      chart.style.display = 'block';
+      chart.src = slide.getAttribute('data-image-src');
+    } else {
+      // Hide the image and show the map for other slides
+      this.map.getContainer().style.display = 'block';
+      const image = document.getElementById('slide-image');
+      image.style.display = 'none';
+      const collection = await this.getSlideFeatureCollection(slide);
+      const layer = this.updateDataLayer(collection, slide.id);
+      /**
      * Create a bounds object from a GeoJSON bbox array.
      * @param {Array} bbox The bounding box of the collection
      * @return {L.latLngBounds} The bounds object
      */
-    const boundsFromBbox = (bbox) => {
-      const [west, south, east, north] = bbox;
-      const bounds = L.latLngBounds(
-          L.latLng(south, west),
-          L.latLng(north, east),
-      );
-      return bounds;
-    };
-
-    /**
+      const boundsFromBbox = (bbox) => {
+        const [west, south, east, north] = bbox;
+        const bounds = L.latLngBounds(
+            L.latLng(south, west),
+            L.latLng(north, east),
+        );
+        return bounds;
+      };
+      /**
      * Create a temporary event handler that will show tooltips on the map
      * features, after the map is done "flying" to contain the data layer.
      */
-    const handleFlyEnd = () => {
-      if (slide.showpopups) {
-        layer.eachLayer((l) => {
-          l.bindTooltip(l.feature.properties.label, { permanent: true });
-          l.openTooltip();
-        });
-      }
-      this.map.removeEventListener('moveend', handleFlyEnd);
-    };
+      const handleFlyEnd = () => {
+        if (slide.showpopups) {
+          layer.eachLayer((l) => {
+            l.bindTooltip(l.feature.properties.label, { permanent: true });
+            l.openTooltip();
+          });
+        }
+        this.map.removeEventListener('moveend', handleFlyEnd);
+      };
 
-    this.map.addEventListener('moveend', handleFlyEnd);
-    if (collection.bbox) {
-      this.map.flyToBounds(boundsFromBbox(collection.bbox));
-    } else {
-      this.map.flyToBounds(layer.getBounds());
+      this.map.addEventListener('moveend', handleFlyEnd);
+      if (collection.bbox) {
+        this.map.flyToBounds(boundsFromBbox(collection.bbox));
+      } else {
+        this.map.flyToBounds(layer.getBounds());
+      }
     }
   }
 
