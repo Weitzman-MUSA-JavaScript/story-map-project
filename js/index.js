b26cde4856d1ef0649c1917fd5f19f1f4fd21bf3
EChinLee <1900016617@pku.edu.cn>
2024-09-23 14:19:19 -0400

    extension updat

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 import { SlideDeck } from './slidedeck.js';
 
 const mapElement = document.querySelector('#map');
-const map = L.map(mapElement).setView([35.652, 139.839], 5);
+var map = L.map(mapElement).setView([35.652, 139.839], 5);
 
-const mapboxKey = 'pk.eyJ1IjoiZWNoaW5saSIsImEiOiJjbTEybWVsY3kwZW1nMmxwbTY4bGx1dDM1In0.Cncmmjeonp1yp1AXZrOqvQ';
-const mapboxStyle = 'echinli/cm1ekw8xv013701pc62pr00jo';
-
+const mapboxKey = 'pk.eyJ1IjoiZWNoaW5saSIsImEiOiJjbTEybWVsY3kwZW1nMmxwbTY4bGx1dDM1In0.Cncmmjeonp1yp1AXZrOqvQ'
+const mapboxStyle = 'echinli/cm1ekw8xv013701pc62pr00jo'
 // ## The Base Tile Layer
 const baseTileLayer = L.tileLayer(`http://api.mapbox.com/styles/v1/${mapboxStyle}/tiles/512/{z}/{x}/{y}?access_token=${mapboxKey}`, {
   tileSize: 512,
