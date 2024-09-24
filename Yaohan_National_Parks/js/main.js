f92c869f98baf4c04fa3fec270d8ffc4a4077dab
YaohanXu <xuyaohan99@gmail.com>
2024-09-24 13:24:35 -0400

    1:24

--- Yaohan_National_Parks/js/main.js
+++ Yaohan_National_Parks/js/main.js
@@ -10,7 +10,7 @@ const map = L.map(element, {scrollWheelZoom: false}).setView([0, 0], 0);
 // Add a base layer to the map
 //
 const mapboxKey = 'pk.eyJ1IjoieHV5YW9oYW4iLCJhIjoiY20xN3l1aDl0MHlhdTJqb3NrN3JzcHZ3ZyJ9.W0K0GomuRMj9lrIY029KoA';
-const mapboxStyle = 'mapbox/dark-v11';
+const mapboxStyle = 'mapbox/light-v11';
 
 const baseLayer = L.tileLayer(
     `https://api.mapbox.com/styles/v1/${mapboxStyle}/tiles/512/{z}/{x}/{y}{r}?access_token=${mapboxKey}`, {
@@ -22,7 +22,9 @@ const baseLayer = L.tileLayer(
 });
 baseLayer.addTo(map);
 
+//
 // ## Interface Elements
+//
 const container = document.querySelector('.slide-section');
 const slides = document.querySelectorAll('.slide');
 
@@ -78,7 +80,9 @@ const slideOptions = {
   },
 };
 
+//
 // ## The SlideDeck object
+//
 const deck = new SlideDeck(container, slides, map, slideOptions);
 
 document.addEventListener('scroll', () => deck.calcCurrentSlideIndex());
