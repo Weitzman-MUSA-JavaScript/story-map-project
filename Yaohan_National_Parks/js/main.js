a76d9276fb8df2011d78addfe001272c39c0e114
YaohanXu <xuyaohan99@gmail.com>
2024-09-24 13:00:43 -0400

    09-24 1:00

--- Yaohan_National_Parks/js/main.js
+++ Yaohan_National_Parks/js/main.js
@@ -1,13 +1,26 @@
 import { SlideDeck } from './slidedeck.js';
 
-const map = L.map('map', {scrollWheelZoom: false}).setView([0, 0], 0);
+//
+// Create a new Leaflet map object to be displayed in the #map div
+//
+const element = document.querySelector('#map');
+const map = L.map(element, {scrollWheelZoom: false}).setView([0, 0], 0);
 
-// ## The Base Tile Layer
-const baseTileLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg', {
-  maxZoom: 16,
-  attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>',
+//
+// Add a base layer to the map
+//
+const mapboxKey = 'pk.eyJ1IjoieHV5YW9oYW4iLCJhIjoiY20xN3l1aDl0MHlhdTJqb3NrN3JzcHZ3ZyJ9.W0K0GomuRMj9lrIY029KoA';
+const mapboxStyle = 'mapbox/dark-v11';
+
+const baseLayer = L.tileLayer(
+    `https://api.mapbox.com/styles/v1/${mapboxStyle}/tiles/512/{z}/{x}/{y}{r}?access_token=${mapboxKey}`, {
+  tileSize: 512,
+  zoomOffset: -1,
+  detectRetina: true,
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
-baseTileLayer.addTo(map);
+baseLayer.addTo(map);
 
 // ## Interface Elements
 const container = document.querySelector('.slide-section');
