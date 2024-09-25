57920e8275a9a9ea6306564d7d0871d866567c2e
avavani <avaniadhikari@hotmail.com>
2024-09-25 12:32:47 -0400

    final_edits

--- drccobalt/js/index.js
+++ drccobalt/js/index.js
@@ -1,17 +1,17 @@
 import { SlideDeck } from './slidedeck.js';
 
 const map = L.map('map', {scrollWheelZoom: false}).setView([0, 0], 5);
-const Mapboxkey = 'pk.eyJ1IjoiYWF2YW5pMTAzIiwiYSI6ImNtMTgxOGkyZzBvYnQyam16bXFydTUwM3QifQ.hXw8FwWysnOw3It_Sms3UQ'
-const Mapboxstyle = 'mapbox/dark-v11'
+const Mapboxkey = 'pk.eyJ1IjoiYWF2YW5pMTAzIiwiYSI6ImNtMTgxOGkyZzBvYnQyam16bXFydTUwM3QifQ.hXw8FwWysnOw3It_Sms3UQ';
+const Mapboxstyle = 'mapbox/dark-v11';
 
 // ## The Base Tile Layer
 const baseTileLayer = L.tileLayer(`https://api.mapbox.com/styles/v1/${Mapboxstyle}/tiles/512/{z}/{x}/{y}{r}?access_token=${Mapboxkey}`, {
-  maxZoom: 16
+  maxZoom: 16,
 });
 
 baseTileLayer.addTo(map);
 
-/// ## Interface Elements
+// / ## Interface Elements
 const container = document.querySelector('.slide-section');
 const slides = document.querySelectorAll('.slide');
 
@@ -33,9 +33,9 @@ const slideOptions = {
         color: 'black',
         weight: 1,
         opacity: 1,
-        fillOpacity: 0.8
+        fillOpacity: 0.8,
       });
-    }
+    },
   },
   'third-slide': {
     pointToLayer: (feature, latlng) => {
@@ -45,9 +45,9 @@ const slideOptions = {
         color: 'black',
         weight: 1,
         opacity: 1,
-        fillOpacity: 0.8
+        fillOpacity: 0.8,
       });
-    }
+    },
   },
   'fourth-slide': {
     pointToLayer: (feature, latlng) => {
@@ -57,9 +57,9 @@ const slideOptions = {
         color: 'black',
         weight: 1,
         opacity: 1,
-        fillOpacity: 0.8
+        fillOpacity: 0.8,
       });
-    }
+    },
   },
   'fifth-slide': {
     style: (feature) => {
@@ -87,9 +87,9 @@ const slideOptions = {
         color: 'black',
         weight: 1,
         opacity: 1,
-        fillOpacity: 0.8
+        fillOpacity: 0.8,
       });
-    }
+    },
   },
   'eight-slide': {
     pointToLayer: (feature, latlng) => {
@@ -99,9 +99,9 @@ const slideOptions = {
         color: 'black',
         weight: 1,
         opacity: 1,
-        fillOpacity: 0.8
+        fillOpacity: 0.8,
       });
-    }
+    },
   },
   'ninth-slide': {
     pointToLayer: (feature, latlng) => {
@@ -111,9 +111,9 @@ const slideOptions = {
         color: 'black',
         weight: 1,
         opacity: 1,
-        fillOpacity: 0.8
+        fillOpacity: 0.8,
       });
-    }
+    },
   },
   'tenth-slide': {
     pointToLayer: (feature, latlng) => {
@@ -123,9 +123,9 @@ const slideOptions = {
         color: 'black',
         weight: 1,
         opacity: 1,
-        fillOpacity: 0.8
+        fillOpacity: 0.8,
       });
-    }
+    },
   },
 };
 
